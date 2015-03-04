a883ce3977251f1bf7ef74d0db3c4f0b10291297
Shintaro Iguchi <iguchi@vitei.com>
2015-03-04 17:12:10 +0900

    Can load very simple model

--- LevelEditorNativeRendering/LvEdRenderingEngine/Model3d/Ayataka/CmdlModelFactory.h
+++ LevelEditorNativeRendering/LvEdRenderingEngine/Model3d/Ayataka/CmdlModelFactory.h
@@ -21,29 +21,15 @@ namespace LvEdEngine
 		void ParseError( const char * fmt, ... );
 		int m_parseErrors;
 
-        void ProcessTexture(Model3dBuilder * builder, xml_node* texNode);
-        xml_node* GetBinding(xml_node* shaderNode, const char* bindingtype, const char* name, const char* partial);
-        void GetColor(xml_node* shaderNode, const char* name, float4* out);
-        void GetPower(xml_node* shaderNode, const char* name, float* out);
-        void ProcessShader(Model3dBuilder * builder, xml_node* shaderNode);
-        void ProcessSources(Model3dBuilder * builder, xml_node* vertexArray);
-        void ProcessPrimitivesFeatures(Model3dBuilder * builder, xml_node* xmlPrim);
-        void ProcessPrimitives(Model3dBuilder * builder, xml_node* xmlPrim, Node * node);
-        void PushCustomDataValue(CustomDataAttribute * customDataAttribute, const char * type, xml_node * xmlValue);
-        void ProcessCustomDataAttributes(xml_node* xmlParent, Node * node);
-        void ProcessMesh(Model3dBuilder * builder, xml_node* xmlMesh, Node * node);
-        void GetTransform(xml_node* node, Matrix * out );
-        void ProcessInstance(Model3dBuilder * builder, xml_node* xmlInstance, Node * node);
-        void ProcessNode(Model3dBuilder * builder, xml_node* xmlNode, Node * parent);
-
 		void ProcessModel( Model3dBuilder* pBuilder, const pugi::xpath_node& modelNode );
 		void ProcessMaterial( Model3dBuilder* pBuilder, const pugi::xpath_node& materialNode );
-		void ProcessMesh( Model3dBuilder* pBuilder, const pugi::xpath_node& meshNodeconst, const pugi::xpath_node_set& shapesNodeSet );
+		void ProcessMesh( Model3dBuilder* pBuilder, Node* pNode, const pugi::xpath_node& meshNode, const pugi::xpath_node_set& shapesNodeSet );
 		void ProcessShape( Model3dBuilder* pBuilder, Node* pNode, Material* pMaterial, const pugi::xpath_node& shapeNode );
+		
 		void ProcessVertexAttributes( Model3dBuilder* pBuilder, const pugi::xpath_node& verticesNode );
+		template <typename T> void ProcessVertexStream( std::vector<T>* out, const pugi::xml_node& node );
 
-		template <typename T>
-		void ProcessVertexStream( std::vector<T>* pos, const pugi::xml_node& child );
 		void ProcessPrimitive( Model3dBuilder* pBuilder, Node* pNode, Material* pMaterial, const pugi::xpath_node& node );
+		template <typename T> void ProcessIndexStream( std::vector<T>* out, const pugi::xml_node& node );
 	};
 };
